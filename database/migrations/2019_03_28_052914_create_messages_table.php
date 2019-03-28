7f3d5789127856831a86c04cf967b5dea41f65ee
1827909101@qq.com <1827909101@qq.com>
2019-03-28 13:30:16 +0800

    添加migrations

---
+++ database/migrations/2019_03_28_052914_create_messages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateMessagesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('messages', function(Blueprint $table)
+		{
+			$table->bigInteger('id', true)->unsigned();
+			$table->string('username')->index();
+			$table->string('userphone')->index();
+			$table->string('postmessage')->index();
+			$table->string('messagessource')->index();
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('messages');
+	}
+
+}
---
+++ database/migrations/2019_03_28_052914_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function(Blueprint $table)
+		{
+			$table->bigInteger('id', true)->unsigned();
+			$table->string('name')->index();
+			$table->string('email')->index();
+			$table->string('password')->index();
+			$table->string('token');
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
